46d770078a21b18c1f2daee40da0cd62a77e7af4
Bager <Bager@BagerdeMacBook-Pro.local>
2017-05-11 09:46:35 +0800

    分享内容修改

--- AiXianFeng/AiXianFeng/ShareManager.swift
+++ AiXianFeng/AiXianFeng/ShareManager.swift
@@ -20,7 +20,7 @@ class ShareManager: NSObject {
         switch shareType {
             
         case .WeiXinMyFriend:
-            UMSocialData.default().extConfig.wechatSessionData.url = blogURLStr
+            UMSocialData.default().extConfig.wechatSessionData.url = GitHubURLString
             UMSocialData.default().extConfig.wechatSessionData.title = "贝格Swift开源新作"
             
             
@@ -39,7 +39,7 @@ class ShareManager: NSObject {
         case .WeiXinCircleOfFriends:
             
             UMSocialData.default().extConfig.wechatTimelineData.url = blogURLStr
-            UMSocialData.default().extConfig.wechatTimelineData.title = "小熊Swift开源新作"
+            UMSocialData.default().extConfig.wechatTimelineData.title = "贝格Swift开源新作"
             let shareURL = UMSocialUrlResource(snsResourceType: UMSocialUrlResourceTypeImage, url: blogURLStr)
             UMSocialDataService.default().postSNS(withTypes: [UMShareToWechatTimeline], content: shareText, image: authorImage, location: nil, urlResource: shareURL, presentedController: nil, completion: { (response) -> Void in
                 if response?.responseCode.rawValue == UMSResponseCodeSuccess.rawValue {
